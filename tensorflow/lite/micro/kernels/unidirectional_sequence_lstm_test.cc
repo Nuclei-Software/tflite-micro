9e07315debe671c17041f9ae51c96159b16451c5
qiujiandong <qiujiandong@nucleisys.com>
2024-07-31 19:22:24 +0800

    tflite-micro: remove unpaired #if

--- tensorflow/lite/micro/kernels/unidirectional_sequence_lstm_test.cc
+++ tensorflow/lite/micro/kernels/unidirectional_sequence_lstm_test.cc
@@ -140,7 +140,6 @@ TF_LITE_MICRO_TEST(TestUnidirectionalLSTMFloat) {
                                                tolerance, float_node_contents);
 }
 
-#if !(defined(CMSIS_NN) || defined(NMSIS_NN))
 TF_LITE_MICRO_TEST(TestUnidirectionalLSTMInt8) {
   const tflite::testing::LstmEvalCheckData<12, 4, 12> kernel_eval_data =
       tflite::testing::Get2X2LstmEvalCheckData();

c017dde8ea7f1a63c0eef702a960517ccb005d22
qiujiandong <qiujiandong@nucleisys.com>
2024-08-07 18:37:00 +0800

    doc: sort run and test result in README.md
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- tensorflow/lite/micro/nuclei_evalsoc/testall.sh
+++ tensorflow/lite/micro/nuclei_evalsoc/testall.sh
@@ -53,9 +53,9 @@ function clean_tflite {
 
 CORE_ARCH=(
     "n205" 
-    "n205 _xxldsp" 
     "n300" 
     "n300 _xxldsp" 
+    "n300fd _xxldspn3x" 
     "n600f" 
     "n600f _zve32f" 
     "n600f _xxldsp" 

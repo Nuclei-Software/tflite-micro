742816b22d60778d92ad29946413297d99c52df9
qiujiandong <qiujiandong@nucleisys.com>
2025-01-17 17:43:37 +0800

    tflite-micro: nmsis_nn kernel adapt to nsdk_0.7.1
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- tensorflow/lite/micro/kernels/conv.h
+++ tensorflow/lite/micro/kernels/conv.h
@@ -103,7 +103,7 @@ inline TFLMRegistration Register_CONV_2D_INT8REF() {
 }
 #endif  // defined(XTENSA)
 
-#if defined(CMSIS_NN)
+#if defined(CMSIS_NN) || defined(NMSIS_NN)
 // Returns a TFLMRegistration struct for kernel variant that only supports
 // int8 activations and int4 weights and uses the latency optimized
 // implementations.
@@ -112,7 +112,7 @@ TFLMRegistration Register_CONV_2D_INT4();
 inline TFLMRegistration Register_CONV_2D_INT4() { return Register_CONV_2D(); }
 #endif  // defined(CMSIS_NN)
 
-#if defined(CMSIS_NN) || defined(XTENSA)
+#if defined(CMSIS_NN) || defined(XTENSA) || defined(NMSIS_NN)
 // Returns a TFLMRegistration struct for kernel variant that only supports
 // int8 activations and int8 weights and uses the latency optimized
 // implementations.
